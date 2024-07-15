4bc7bdb05f828746c7ca9c6cb1ac89966dce9a41
Protonosgit <linuslinz98@gmail.com>
2024-07-15 18:08:00 +0200

    init

---
+++ MMM-PingTime.js
@@ -0,0 +1,90 @@
+
+
+Module.register("MMM-PingTime", {
+    defaults: {
+        updateInterval: 6000,
+        websocketUrl: "wss://echo.websocket.org"
+    },
+
+    getTemplate: function() {
+        return "MMM-PingTime.njk";
+    },
+
+    getTemplateData: function() {
+        return {
+            pingResult: this.pingResult,
+            config: this.config
+        };
+    },
+
+    start: function() {
+        console.log("Starting module: " + this.name);
+        this.updateDom();
+
+        setTimeout(() => {
+            this.connect();
+        }, 2000);
+    },
+
+    connect: function() {
+        this.socket = new WebSocket(this.config.websocketUrl);
+
+        this.socket.onopen = () => {
+            console.log("WebSocket connected");
+            this.pingResult = "Connected";
+            this.updateDom();
+            this.startPinging();
+        };
+
+        this.socket.onclose = () => {
+            console.log("WebSocket disconnected");
+            this.pingResult = "Disconnected";
+            this.pingStartTime = 0;
+            this.updateDom();
+            this.stopPinging();
+            // Try to reconnect after a delay
+            setTimeout(() => {
+                this.connect();
+            }, 5000);
+        };
+
+        this.socket.onmessage = (event) => {
+            const latency = Date.now() - this.pingStartTime;
+            if(event.data === "ping") {
+                this.pingResult = latency + " ms";
+            }
+            this.updateDom();
+        };
+    },
+
+    startPinging: function() {
+        console.log("Starting pinging");
+        this.pingInterval = setInterval(() => {
+            this.ping();
+        }, this.config.updateInterval);
+    },
+
+    stopPinging: function() {
+        if (this.pingInterval) {
+            clearInterval(this.pingInterval);
+        }
+    },
+
+    ping: function() {
+        if (this.socket && this.socket.readyState === WebSocket.OPEN) {
+            this.pingStartTime = Date.now();
+            this.socket.send("ping");
+        }
+    },
+
+    suspend: function() {
+        this.stopPinging();
+        if (this.socket) {
+            this.socket.close();
+        }
+    },
+
+    resume: function() {
+        this.connect();
+    }
+});
